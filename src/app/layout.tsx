c926da06335714381a7c43265592d13ce846b1e1
Jordan <jordanpraissman@gmail.com>
2024-08-23 20:46:08 -0400

    Added Google Anayltics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import NavBar from "../components/NavBar";
 import WebsiteFooter from "../components/WebsiteFooter";
 import { Box } from "@mui/material";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-20R9LPJNBJ"
+        ></Script>
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-20R9LPJNBJ');`}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Box sx={{ minHeight: "100vh" }}>
           <NavBar />
