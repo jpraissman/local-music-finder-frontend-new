9df58d99a9c02a71c78ffee9c990c2c76095fe0b
Jordan <jordanpraissman@gmail.com>
2024-11-26 14:54:32 -0500

    Reverted middle ware changes back

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,69 +1,10 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const response = NextResponse.next();
-
-  // Get the fromCookie (and set it if it doesn't exist)
-  let fromCookieValue = getExistingFromCookie(req);
-  if (fromCookieValue === undefined || fromCookieValue == "Unknown") {
-    fromCookieValue = getFromCookieInURL(req);
-    response.cookies.set('from', fromCookieValue, { path: '/' });
-  }
-
-  let userIdCookieValue = undefined;
-  let userIdCookie = req.cookies.get('user');
-  if (userIdCookie === undefined) {
-    userIdCookieValue = generateRandomId();
-    response.cookies.set('user', userIdCookieValue, { path: '/', maxAge: 10 * 365 * 24 * 60 * 60 });
-  }
-  else {
-    userIdCookieValue = userIdCookie.value;
-  }
-
-  const visitBody = {
-    path: req.nextUrl.pathname,
-    from_where: fromCookieValue,
-    user_id: userIdCookieValue,
-  }
-  fetch(process.env.NEXT_PUBLIC_API_BASE_URL + '/visit', {
-    method: 'POST',
-    body: JSON.stringify(visitBody),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).catch((error) => {
-    // Log error, but don't block the middleware
-    console.error('Error making API call:', error);
-  });
-
-  return response;
-}
-
-function generateRandomId(length: number = 10): string {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
   
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters[randomIndex];
-  }
-  
-  return result;
-}
-
-function getExistingFromCookie(req: NextRequest): string | undefined {
-  const fromCookie = req.cookies.get('from');
-  if (fromCookie === undefined) {
-    return undefined;
-  }
-
-  return fromCookie.value;
-}
-
-function getFromCookieInURL(req: NextRequest): string {
-  // Return "Unknown" if there is no from param in the URL 
+  // Return if there is no search params in the url
   if (req.nextUrl.pathname.startsWith('/_next') || req.nextUrl.search.length < 1) {
-    return "Unknown";
+    return NextResponse.next();
   }
 
   // Get the "from" search param
@@ -77,23 +18,16 @@ function getFromCookieInURL(req: NextRequest): string {
     }
   }
 
-  if (fromCookieValue === null) {
-    return "Unknown";
+  // Set a cookie based on the query parameter
+  const response = NextResponse.next();
+  if (fromCookieValue !== null) {
+    response.cookies.set('from', fromCookieValue, { path: '/', maxAge: 60 * 60 * 24 });
   }
 
-  return fromCookieValue;
+  return response;
 }
 
 // Apply middleware to all routes
 export const config = {
-  matcher: [
-    '/',
-    '/find/:path*',
-    '/post',
-    '/edit',
-    '/faqs',
-    '/about-us',
-    '/privacy-policy',
-    '/terms'
-  ],
+  matcher: '/:path*',
 };
