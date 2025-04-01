27a66a4cbafb99ef544c139a0fd3023cc319542e
Jordan <jordanpraissman@gmail.com>
2025-04-01 00:03:32 -0400

    Can now query multiple counties

--- src/app/events/county/[countyNames]/page.tsx
+++ src/app/events/county/[countyNames]/page.tsx
@@ -6,30 +6,36 @@ import { Metadata } from "next";
 
 interface PageProps {
   params: {
-    countyName: string;
+    countyNames: string;
   };
 }
 
 export function generateMetadata({
-  params: { countyName },
+  params: { countyNames },
 }: PageProps): Metadata {
   return {
-    title: `Live Music Events In ${countyName} - The Local Music Finder`,
+    title: `Live Music Events In ${countyNames
+      .replaceAll("%20", " ")
+      .split("%3A%3A")
+      .join(", ")} - The Local Music Finder`,
   };
 }
 
 export const revalidate = 0;
 
-export default async function Page({ params: { countyName } }: PageProps) {
+export default async function Page({ params: { countyNames } }: PageProps) {
   let events: Event[] = [];
   try {
-    const response = await fetch(
-      process.env.NEXT_PUBLIC_API_BASE_URL + `/events/county/${countyName}`
-    );
-    if (response.ok) {
-      const eventsRaw = await response.json();
-      events = eventsRaw.events;
-    }
+    const countyNamesSplit = countyNames.split("%3A%3A");
+    countyNamesSplit.forEach(async (county) => {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_API_BASE_URL + `/events/county/${county}`
+      );
+      if (response.ok) {
+        const eventsRaw = await response.json();
+        events.push(eventsRaw.events);
+      }
+    });
   } catch (error) {}
 
   return (
@@ -49,7 +55,10 @@ export default async function Page({ params: { countyName } }: PageProps) {
         eventsInit={events}
         noFilters={false}
         landingPage={true}
-        searchLocation={countyName.replace("%20", " ")}
+        searchLocation={countyNames
+          .replaceAll("%20", " ")
+          .split("%3A%3A")
+          .join(", ")}
         searchDateRange="in the next month"
       />
     </>
