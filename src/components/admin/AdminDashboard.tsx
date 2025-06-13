e23fae89812f420574c42dd1d8ec9fd1d1f68a6a
Jordan <jordanpraissman@gmail.com>
2025-06-12 21:37:23 -0400

    add sorting to admin dhasboard

--- src/components/admin/AdminDashboard.tsx
+++ src/components/admin/AdminDashboard.tsx
@@ -15,9 +15,9 @@ import {
   Typography,
 } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { all } from "axios";
 import dayjs from "dayjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DateRange, DayPicker } from "react-day-picker";
 import StatCard from "./StatCard";
 import Link from "next/link";
@@ -30,6 +30,46 @@ interface FetchUserDataProps {
   adminKey: string;
 }
 
+interface AdminData {
+  users: {
+    [key: string]: {
+      user_id: string;
+      duration: number;
+      device: string;
+      referer: string;
+      videos_clicked: number;
+      events_viewed: number;
+      type: number;
+      pages_visited: number;
+      venues_viewed: number;
+      bands_viewed: number;
+      start_time: string;
+    };
+  };
+  totals: {
+    total_users: number;
+    total_new_users: number;
+    total_returning_users: number;
+    total_sessions: number;
+    total_new_sessions: number;
+    total_returning_sessions: number;
+    total_mobile_users: number;
+    total_tablet_users: number;
+    total_computer_users: number;
+    total_facebook_referers: number;
+    total_reddit_referers: number;
+    total_google_referers: number;
+    total_patch_referers: number;
+    total_unknown_referers: number;
+    total_videos_clicked: number;
+    total_events_viewed: number;
+    total_pages_visited: number;
+    total_duration: number;
+    total_venues_viewed: number;
+    total_bands_viewed: number;
+  };
+}
+
 async function fetchUserData({
   fromDate,
   toDate,
@@ -40,45 +80,7 @@ async function fetchUserData({
   const res = await axios.get(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/users?admin_key=${adminKey}&from_date=${fromDate}&to_date=${toDate}&include_admins=${includeAdmins}&filter_out_zero_duration=${filterOutZeroSecondDurations}`
   );
-  const data: {
-    users: {
-      [key: string]: {
-        user_id: string;
-        duration: number;
-        device: string;
-        referer: string;
-        videos_clicked: number;
-        events_viewed: number;
-        type: number;
-        pages_visited: number;
-        venues_viewed: number;
-        bands_viewed: number;
-        start_time: string;
-      };
-    };
-    totals: {
-      total_users: number;
-      total_new_users: number;
-      total_returning_users: number;
-      total_sessions: number;
-      total_new_sessions: number;
-      total_returning_sessions: number;
-      total_mobile_users: number;
-      total_tablet_users: number;
-      total_computer_users: number;
-      total_facebook_referers: number;
-      total_reddit_referers: number;
-      total_google_referers: number;
-      total_patch_referers: number;
-      total_unknown_referers: number;
-      total_videos_clicked: number;
-      total_events_viewed: number;
-      total_pages_visited: number;
-      total_duration: number;
-      total_venues_viewed: number;
-      total_bands_viewed: number;
-    };
-  } = res.data;
+  const data: AdminData = res.data;
 
   return data;
 }
@@ -123,6 +125,52 @@ export default function AdminDashboard({ adminKey }: AdminDashboardProps) {
       }),
   });
 
+  const [allData, setAllData] = useState<AdminData | undefined>(undefined);
+
+  useEffect(() => {
+    setAllData(data);
+  }, [data]);
+
+  const sortUsers = (
+    key:
+      | "user_id"
+      | "duration"
+      | "device"
+      | "referer"
+      | "videos_clicked"
+      | "events_viewed"
+      | "type"
+      | "pages_visited"
+      | "venues_viewed"
+      | "bands_viewed"
+      | "start_time"
+  ) => {
+    if (allData) {
+      const userDataSorted = Object.entries(allData.users).sort(
+        (user1, user2) => {
+          if (
+            typeof user1[1][key] === "string" &&
+            typeof user2[1][key] === "string"
+          ) {
+            return user1[1][key].localeCompare(user2[1][key]);
+          } else if (
+            typeof user1[1][key] === "number" &&
+            typeof user2[1][key] === "number"
+          ) {
+            return user1[1][key] - user2[1][key];
+          } else {
+            return 1;
+          }
+        }
+      );
+
+      setAllData({
+        users: Object.fromEntries(userDataSorted),
+        totals: allData.totals,
+      });
+    }
+  };
+
   return (
     <Box sx={{ paddingTop: "50px", display: "flex", justifyContent: "center" }}>
       <Stack direction={"column"} spacing={5}>
@@ -163,144 +211,223 @@ export default function AdminDashboard({ adminKey }: AdminDashboardProps) {
             </Stack>
           </Box>
         </Stack>
-        {data && (
+        {allData && (
           <Stack direction={"column"} spacing={3}>
             <Stack direction={"row"} spacing={4}>
               <StatCard
                 title="Total Unique Users"
-                value={data.totals.total_users}
+                value={allData.totals.total_users}
               />
               <StatCard
                 title="New Unique Users"
-                value={data.totals.total_new_users}
+                value={allData.totals.total_new_users}
               />
               <StatCard
                 title="Returning Unique Users"
-                value={data.totals.total_returning_users}
+                value={allData.totals.total_returning_users}
               />
             </Stack>
             <Stack direction={"row"} spacing={4}>
               <StatCard
                 title="Total Sessions"
-                value={data.totals.total_sessions}
+                value={allData.totals.total_sessions}
               />
               <StatCard
                 title="New Sessions"
-                value={data.totals.total_new_sessions}
+                value={allData.totals.total_new_sessions}
               />
               <StatCard
                 title="Returning Sessions"
-                value={data.totals.total_returning_sessions}
+                value={allData.totals.total_returning_sessions}
               />
             </Stack>
             <Stack direction={"row"} spacing={4}>
               <StatCard
                 title="% Mobile"
-                value={data.totals.total_mobile_users / data.totals.total_users}
+                value={
+                  allData.totals.total_mobile_users / allData.totals.total_users
+                }
               />
               <StatCard
                 title="% Computer"
                 value={
-                  data.totals.total_computer_users / data.totals.total_users
+                  allData.totals.total_computer_users /
+                  allData.totals.total_users
                 }
               />
               <StatCard
                 title="% Tablet"
-                value={data.totals.total_tablet_users / data.totals.total_users}
+                value={
+                  allData.totals.total_tablet_users / allData.totals.total_users
+                }
               />
             </Stack>
             <Stack direction={"row"} spacing={4}>
               <StatCard
                 title="% Facebook"
                 value={
-                  data.totals.total_facebook_referers / data.totals.total_users
+                  allData.totals.total_facebook_referers /
+                  allData.totals.total_users
                 }
               />
               <StatCard
                 title="% Reddit"
                 value={
-                  data.totals.total_reddit_referers / data.totals.total_users
+                  allData.totals.total_reddit_referers /
+                  allData.totals.total_users
                 }
               />
               <StatCard
                 title="% Google"
                 value={
-                  data.totals.total_google_referers / data.totals.total_users
+                  allData.totals.total_google_referers /
+                  allData.totals.total_users
                 }
               />
               <StatCard
                 title="% Patch"
                 value={
-                  data.totals.total_patch_referers / data.totals.total_users
+                  allData.totals.total_patch_referers /
+                  allData.totals.total_users
                 }
               />
               <StatCard
                 title="% Unknown"
                 value={
-                  data.totals.total_unknown_referers / data.totals.total_users
+                  allData.totals.total_unknown_referers /
+                  allData.totals.total_users
                 }
               />
             </Stack>
             <Stack direction={"row"} spacing={4}>
               <StatCard
                 title="Avg Duration"
-                value={data.totals.total_duration / data.totals.total_users}
+                value={
+                  allData.totals.total_duration / allData.totals.total_users
+                }
               />
               <StatCard
                 title="Avg Pages Visited"
                 value={
-                  data.totals.total_pages_visited / data.totals.total_users
+                  allData.totals.total_pages_visited /
+                  allData.totals.total_users
                 }
               />
               <StatCard
                 title="Avg Events Viewed"
                 value={
-                  data.totals.total_events_viewed / data.totals.total_users
+                  allData.totals.total_events_viewed /
+                  allData.totals.total_users
                 }
               />
               <StatCard
                 title="Total Videos Clicked"
-                value={data.totals.total_videos_clicked}
+                value={allData.totals.total_videos_clicked}
               />
             </Stack>
             <Stack direction={"row"} spacing={4}>
               <StatCard
                 title="Total Venues Viewed"
-                value={data.totals.total_venues_viewed}
+                value={allData.totals.total_venues_viewed}
               />
               <StatCard
                 title="Total Bands Viewed"
-                value={data.totals.total_bands_viewed}
+                value={allData.totals.total_bands_viewed}
               />
             </Stack>
             <TableContainer component={Paper}>
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                   <TableRow>
-                    <TableCell>User Id</TableCell>
-                    <TableCell align="right">Duration</TableCell>
-                    <TableCell align="right">Start Time</TableCell>
-                    <TableCell align="right">Device</TableCell>
-                    <TableCell align="right">Referer</TableCell>
-                    <TableCell align="right">Type</TableCell>
-                    <TableCell align="right">Videos Clicked</TableCell>
-                    <TableCell align="right">Events Viewed</TableCell>
-                    <TableCell align="right">Pages Visited</TableCell>
-                    <TableCell align="right">Venues Viewed</TableCell>
-                    <TableCell align="right">Bands Viewed</TableCell>
+                    <TableCell
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("user_id")}
+                    >
+                      User Id
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("duration")}
+                    >
+                      Duration
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("start_time")}
+                    >
+                      Start Time
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("device")}
+                    >
+                      Device
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("referer")}
+                    >
+                      Referer
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("type")}
+                    >
+                      Type
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("videos_clicked")}
+                    >
+                      Videos Clicked
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("events_viewed")}
+                    >
+                      Events Viewed
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("pages_visited")}
+                    >
+                      Pages Visited
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("venues_viewed")}
+                    >
+                      Venues Viewed
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ cursor: "pointer" }}
+                      onClick={() => sortUsers("bands_viewed")}
+                    >
+                      Bands Viewed
+                    </TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {Object.entries(data.users).map((user) => (
+                  {Object.entries(allData.users).map((user) => (
                     <TableRow
                       key={user[1].user_id}
                       sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                     >
-                      <Link href={`/admin/user/${user[1].user_id}`}>
-                        <TableCell component="th" scope="row">
+                      <TableCell component="th" scope="row">
+                        <Link href={`/admin/user/${user[1].user_id}`}>
                           {user[1].user_id}
-                        </TableCell>
-                      </Link>
+                        </Link>
+                      </TableCell>
                       <TableCell align="right">{user[1].duration}</TableCell>
                       <TableCell align="right">
                         {dayjs(user[1].start_time.replace("GMT", "")).format(
