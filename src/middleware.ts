567a84abac7db1197dd6623d141ccaf8b7178dbc
Jordan <jordanpraissman@gmail.com>
2025-12-21 13:02:28 -0500

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,49 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const referrer = req.headers.get("referer");
+const ONE_YEAR = 60 * 60 * 24 * 365;
+const TEN_MINUTES = 60 * 10;
 
-  if (referrer && !referrer.includes("thelocalmusicfinder")) {
-    console.log("REFERRER:", referrer);
+export function middleware(req: NextRequest) {
+  const response = NextResponse.next();
+
+  const userIdCookie = req.cookies.get("userId_v2");
+  if (!userIdCookie) {
+    // if no userIdCookie, set one
+    response.cookies.set("userId_v2", crypto.randomUUID(), {
+      maxAge: ONE_YEAR,
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+    });
+  } else if (Math.random() < 0.05) {
+    // otherwsie update every 20 requests to keep alive
+    response.cookies.set("userId_v2", userIdCookie.value, {
+      maxAge: ONE_YEAR,
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+    });
   }
 
-  return NextResponse.next();
+  const referrer = req.headers.get("referer");
+  if (referrer && !referrer.includes("thelocalmusicfinder")) {
+    response.cookies.set("referrer_v2", referrer, {
+      maxAge: TEN_MINUTES,
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+    });
+    response.cookies.set("urlEntry_v2", req.nextUrl.pathname, {
+      maxAge: TEN_MINUTES,
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+    });
+  }
+
+  return response;
 }
